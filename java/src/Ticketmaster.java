3a5eea713921d4781f85900b698f5935b0ccdeaa
Jenaro <jvega008@ucr.edu>
2020-06-13 19:03:39 -0700

    &&

--- java/src/Ticketmaster.java
+++ java/src/Ticketmaster.java
@@ -895,7 +895,7 @@ public class Ticketmaster{
 
 			try{
 				input = in.readLine();
-				if(!input.equals("n") || !input.equals("y")){
+				if(!input.equals("n") && !input.equals("y")){
 					throw new RuntimeException("Invalid input! Enter (y/n)");
 				}
 				break;
